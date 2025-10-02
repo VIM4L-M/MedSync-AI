ee44dd6f693489f1feaef155a18c7237fdfbe450
MNNITAK <ay9120991471@gmail.com>
2025-10-02 15:34:35 +0530

    initialised the node cron back service for notification

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import MedicineRoutes from './routes/medicineRoutes.js';
+import authRoutes from "../src/routes/auth.js";
+import startNotificationScheduler from "./api/notificationController.js";
 
-dotenv.config();
+dotenv.config({path: '../.env'});
 const app = express();
 
 const allowedOrigins = [
@@ -18,14 +20,21 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
+app.use("/api/auth", authRoutes);
 
 app.use("/api/medicine",MedicineRoutes );
 // Test route
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
+ 
+const start=async()=>{
 
-mongoose.connect("mongodb+srv://heyanushkagupta_db_user:qySDIVIDo5v7Blgf@cluster0.ttdy0sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+await mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(process.env.PORT || 8080, () => console.log("Server running")))
   .catch(err => console.error(err));
+
+startNotificationScheduler();
+
+}
+start();
