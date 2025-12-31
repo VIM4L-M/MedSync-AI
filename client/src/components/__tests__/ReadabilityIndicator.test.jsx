fb8dbc85d8acb0110b44c853af7894277e3f6bc9
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-12-31 13:40:55 +0300

    Fix import path for ReadabilityIndicator component in test file

--- client/src/components/__tests__/ReadabilityIndicator.test.jsx
+++ client/src/components/__tests__/ReadabilityIndicator.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import ReadabilityIndicator from '../../ReadabilityIndicator';
+import ReadabilityIndicator from '../ReadabilityIndicator.jsx';
 
 describe('ReadabilityIndicator Component', () => {
   describe('rendering', () => {
